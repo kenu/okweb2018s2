6cb6938fdb76516a5d34780594e5b79f76be3e8c
kenu <kenu.heo@gmail.com>
2018-04-21 15:23:44 +0900

    chat

--- 01node/first.js
+++ 01node/first.js
@@ -1,5 +1,5 @@
 var http = require('http');
-http.createServer(function (req, res) {
+http.createServer( (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('<h1>Hello World\n</h1>');
 }).listen(1337, "127.0.0.1");
